c5689c90d42af2ada4e812c67a15bba56d7ceffd
Erik Bedard <erikbedard@uvic.ca>
2022-04-10 14:10:25 -0700

    Remove lint, update decrypt.sh exec path.

--- scripts/train.sh
+++ scripts/train.sh
@@ -10,15 +10,17 @@
 # arg1: task num
 # arg2: network [2d, 3d_fullres, 3d_lowres, 3d_cascade_fullres]
 # arg3: fold [0, 1, 2, 3, 4]
+# arg4: password for decryption (if needed)
 TASK_NUM=$1
 NET=$2
 FOLD=$3
+PASSWORD=$4
 
 
 # create unique dir for this task
 NUM="$(date +"%s")"
 TASK_DIR=$SLURM_TMPDIR/$NUM
-mkdir $TASK_DIR
+mkdir "$TASK_DIR"
 
 
 # configure task for nnU-Net
@@ -26,36 +28,36 @@ export nnUNet_raw_data_base=$TASK_DIR/nnUNet_raw_data_base
 export nnUNet_preprocessed=$TASK_DIR/nnUNet_preprocessed
 export RESULTS_FOLDER=/project/def-branzana/ebedard/nnUNet_trained_models
 
-mkdir $nnUNet_raw_data_base
-mkdir $nnUNet_raw_data_base/nnUNet_raw_data
-mkdir $nnUNet_preprocessed
+mkdir "$nnUNet_raw_data_base"
+mkdir "$nnUNet_raw_data_base"/nnUNet_raw_data
+mkdir "$nnUNet_preprocessed"
 
 
 # duplicate environment on local machine
 SOURCE_DIR_TEMP=$TASK_DIR/source
-mkdir $SOURCE_DIR_TEMP
-cd $SOURCE_DIR_TEMP
+mkdir "$SOURCE_DIR_TEMP"
+cd "$SOURCE_DIR_TEMP"
 git clone https://github.com/erikbedard/nnUNet-1.git
 
 VENV_TEMP=$TASK_DIR/venv/nnUNet-temp
-virtualenv --no-download $VENV_TEMP
-source $VENV_TEMP/bin/activate
-pip install --no-index -r $SOURCE_DIR_TEMP/nnUNet-1/requirements-cc.txt
-pip install -e $SOURCE_DIR_TEMP/nnUNet-1
+virtualenv --no-download "$VENV_TEMP"
+source "$VENV_TEMP"/bin/activate
+pip install --no-index -r "$SOURCE_DIR_TEMP"/nnUNet-1/requirements-cc.txt
+pip install -e "$SOURCE_DIR_TEMP"/nnUNet-1
 
 
 # retrieve data path and make local copy
 SOURCE_DATA_DIR=/project/def-branzana/ebedard/data
-read DATA_ITEM_PATH __ <<< $(find $SOURCE_DATA_DIR -maxdepth 1 -name "*$TASK_NUM*")
-DATA_ITEM_BASE=$(basename $DATA_ITEM_PATH)
+read DATA_ITEM_PATH __ <<< "$(find $SOURCE_DATA_DIR -maxdepth 1 -name "*$TASK_NUM*")"
+DATA_ITEM_BASE=$(basename "$DATA_ITEM_PATH")
 LOCAL_DATA_DIR=$nnUNet_raw_data_base/nnUNet_raw_data
-cp -r $DATA_ITEM_PATH $LOCAL_DATA_DIR
+cp -r "$DATA_ITEM_PATH" "$LOCAL_DATA_DIR"
 
 
 # decrypt if encrypted
-if [ $DATA_ITEM_BASE == *.tar.gpg ]; then
-cd $LOCAL_DATA_DIR
-source /home/ebedard/scripts/decrypt.sh $DATA_ITEM_BASE "OT&BShoulders"
+if [[ "$DATA_ITEM_BASE" == *.tar.gpg ]]; then
+cd "$LOCAL_DATA_DIR"
+source "$SOURCE_DIR_TEMP"/nnUNet-1/scripts/decrypt.sh "$DATA_ITEM_BASE" "$PASSWORD"
 fi
 
 
